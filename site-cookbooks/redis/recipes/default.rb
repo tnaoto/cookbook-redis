252d5101904c9ad1c7611a42e781cd2166b48a4b
Naoto TAKAHASHI <tnaoto@gmail.com>
2013-08-16 00:58:32 +0900

    change install

--- site-cookbooks/redis/recipes/default.rb
+++ site-cookbooks/redis/recipes/default.rb
@@ -7,17 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "/usr/local/src/redis-2.6.9.tar.gz" do
-        source "http://redis.googlecode.com/files/redis-2.6.9.tar.gz"
-        notifies :run, "bash[install_program]", :immediately
-end
-
-bash "install_program" do
-        not_if "ls /usr/local/bin/redis-server"
-        user "root"
-        cwd "/usr/local/src"
-        code <<-EOH
-                tar -zxf redis-2.6.9.tar.gz
-                (cd redis-2.6.9/ && make test && make && make install)
-        EOH
+package "redis"  do
+	action :install
 end
